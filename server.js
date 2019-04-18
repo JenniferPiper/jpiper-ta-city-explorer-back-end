4fc0d1d5013acbfdb179405dbf1bd619b14b16a3
Jennifer Piper <jennifer@squareandplum.com>
2019-04-17 17:16:00 -0700

    install superagent, refactor /location route to use google geocode api

--- server.js
+++ server.js
@@ -5,18 +5,21 @@ require('dotenv').config();
 const PORT = process.env.PORT || 4000;
 const express = require('express');
 const cors = require('cors');
+const superagent = require('superagent');
 
 const app = express();
 app.use(cors());
 
 app.get('/location', (request, response) => {
-  const locationData = searchLatLng(request.query.data);
-  response.send(locationData);
+  console.log('request.query:', request.query);
+  searchLatLng(request.query.data)
+    .then(location => response.send(location))
+    .catch(error => handleError(error, response));
 });
 
 app.get('/weather', (request, response) => {
   const locationData = searchLatLng(request.query.data);
-  const weatherData = searchWeather(locationData.latitude, locationData.longitude);
+  const weatherData = getWeather(locationData.latitude, locationData.longitude);
   response.send(weatherData);
 })
 
@@ -25,25 +28,28 @@ app.use('*', (request, response) => {
 });
 
 function searchLatLng(frontEndQuery) {
-  const search_query = frontEndQuery;
-  const testData = require('./data/geo.json');
-  const formatted_query = testData.results[0].formatted_address;
-  const latitude = testData.results[0].geometry.location.lat;
-  const longitude = testData.results[0].geometry.location.lng;
-  const responseObject = {
-    search_query,
-    formatted_query,
-    latitude,
-    longitude
-  };
-  return responseObject;
+  const url = `https://maps.googleapis.com/maps/api/geocode/json?address=${frontEndQuery}&key=${process.env.GEOCODE_API_KEY}`;
+
+  return superagent.get(url)
+    .then(result => {
+      console.log('result.BODY: ', result.body);
+      return new Location(frontEndQuery, result);
+    })
+    .catch(error => console.log('JPiper city explorer error: ', error));
 }
 
-function searchWeather(latitude, longitude) {
+function Location(query, res) {
+  this.search_query = query;
+  this.formatted_query = res.body.results[0].formatted_address;
+  this.latitude = res.body.results[0].geometry.location.lat;
+  this.longitude = res.body.results[0].geometry.location.lng;
+}
+
+function getWeather(latitude, longitude) {
   const testWeatherData = require('./data/darksky.json');
   const dailyWeatherData = testWeatherData.daily.data;
 
-  const weatherObjects = dailyWeatherData.map( dayObj => {
+  const weatherObjects = dailyWeatherData.map(dayObj => {
     return new DailyWeather(dayObj);
   });
 
@@ -54,6 +60,11 @@ function DailyWeather(rawDayObj) {
   const time = new Date(rawDayObj.time * 1000).toDateString();
   this.forecast = rawDayObj.summary,
   this.time = time
+}
+// Error handler
+function handleError(err, res) {
+  console.error('JPiper city explorer error: ', err);
+  if (res) res.status(500).send('Sorry, something went wrong');
 }
 
 app.listen(PORT, () => {
